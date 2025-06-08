4b46802b3b2920ca7dffcf4d2046460fd1bc3e6a
agf29 <119333624+tolm1@users.noreply.github.com>
2025-06-08 12:28:55 +0300

    Update считаем количество оборотов.ino

--- считаем количество оборотов.ino
+++ считаем количество оборотов.ino
@@ -5,19 +5,13 @@ unsigned long timeold = 0;
 
 void setup ()
 {
-  //pinMode (13, OUTPUT); // built-in LED pin set to output
   pinMode (8, INPUT);   // module digital output connected to Arduino pin 8
   Serial.begin(9600);   // initialize serial
 }
 void loop ()
 {
-  //Serial.println(rm); // display analog and digital values to serial
-  //Serial.println(rm*60.0*1000.0/(millis() - timeold));
-  //Serial.println(millis() - timeold);
   String data = String(rm*60.0*1000.0/(millis()));
-  //Serial.println(data);
   Serial.println(data + "," +millis() );
-  //Serial.println();
   if (digitalRead(8) == HIGH) {
     rm++;
     timeold = millis();
@@ -26,6 +20,5 @@ void loop ()
     timeold = millis();
     rm = 0;
   }
-  //timeold = millis();
-  delay(50); // wait 100 milliSeconds
+  delay(50); // wait 50 milliSeconds
 }
